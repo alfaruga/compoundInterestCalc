29d91bb628fdffb114ed4d67563f8f45340ef3ba
alexisRuiz <alfaruga@gmail.com>
2022-05-25 10:34:26 -0500

    Updates

---
+++ precache-manifest.29baff7a109e54896dc89840e85f698e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cde3bf1857ce2c5a71cc",
+    "url": "/alfaruga/compoundInterestCalc/static/css/main.b6ecf46a.chunk.css"
+  },
+  {
+    "revision": "cde3bf1857ce2c5a71cc",
+    "url": "/alfaruga/compoundInterestCalc/static/js/main.cde3bf18.chunk.js"
+  },
+  {
+    "revision": "e7f1514e5d4b874b50f9",
+    "url": "/alfaruga/compoundInterestCalc/static/js/1.e7f1514e.chunk.js"
+  },
+  {
+    "revision": "84419f7495f2e8c940a8",
+    "url": "/alfaruga/compoundInterestCalc/static/js/runtime~main.84419f74.js"
+  },
+  {
+    "revision": "273e4aad907858fe44a9d6c0e07a0efd",
+    "url": "/alfaruga/compoundInterestCalc/index.html"
+  }
+];
